1042d3962ba48b234757b91b0c8e5240c8ca8c61
ahmed-asiri <ahmed.su.asiri@gmail.com>
2023-02-15 10:31:45 +0300

    add app logic

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 (function() {
     const formElement = document.querySelector(".rating-card__form");
+    const ratingSection = document.querySelector(".rating-card");
+    const thankYouSection = document.querySelector(".thank-you");
+    const resultEle = document.querySelector(".thank-you__result-value");
     let selectedRating = 0;
 
     formElement.addEventListener("submit", (eve) => {
         eve.preventDefault();
         selectedRating = getSelectedRating();
+        console.log(selectedRating);
+        if(selectedRating) {
+            activateThankYouSection();
+            updateResultElement(selectedRating);
+        }
     })
 
 
@@ -15,6 +23,15 @@
             selectedRating = selectedRadioBtn.value;
         }
         return selectedRating;
+    }
+
+    function activateThankYouSection() {
+        ratingSection.classList.add("d-none");
+        thankYouSection.classList.remove("d-none");
+    }
+
+    function updateResultElement(selectedRating) {
+        resultEle.textContent = selectedRating;
     }
 
 })()
